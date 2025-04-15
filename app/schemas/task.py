b9ee7230481b59e221959f2521f5761fe5e4fd01
idalz <idalez@outlook.com>
2025-04-15 13:09:20 +0300

    Add schemas for Task model

---
+++ app/schemas/task.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+# Bae schema for reading and creating tasks
+class TaskBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+
+# Schema for creating a task 
+class TaskCreate(TaskBase):
+    pass
+
+# Schema for reading a task
+class TaskRead(TaskBase):
+    id: int
+
+    # Make it a response model
+    class Config:
+        orm_mode = True
