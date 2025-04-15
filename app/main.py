3b1217bb229cfcd8dcfd5ccb8b9b0c8badcb85d1
idalz <idalez@outlook.com>
2025-04-15 13:49:45 +0300

    Integrate CRUD Operations with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import models, schemas, crud, database
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Task Manager API"}
+
+# Dependency to get the DB session
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Create a new task
+@app.post("/tasks/", response_model=schemas.TaskRead)
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return crud.create_task(db=db, task=task)
+
+# Get all tasks
+@app.get("/tasks/", response_model=list[schemas.TaskRead])
+def  get_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tasks = crud.get_tasks(db=db, skip=skip, limit=limit)
+    return tasks
+
+# Get a single task by ID
+@app.get("/tasks/{task_id}", response_model=schemas.TaskRead) 
+def get_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.get_task(db=db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+    
