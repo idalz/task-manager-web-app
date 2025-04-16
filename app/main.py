451ad187401d47a673f377e9b09cb47778439e4f
idalz <idalez@outlook.com>
2025-04-16 13:51:48 +0300

    Move task routes from main.py to routes folder

--- app/main.py
+++ app/main.py
@@ -1,53 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app import database
-from app.models import task as models
-from app.schemas import task as schemas
-from app.crud import task as crud
+from fastapi import FastAPI
+from app.routes import task as routes
 
 
 app = FastAPI()
 
-# Dependency to get the DB session
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# Create a new task
-@app.post("/tasks/", response_model=schemas.TaskRead)
-def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    return crud.create_task(db=db, task=task)
-
-# Update a task
-@app.put("/tasks/{task_id}", response_model=schemas.TaskRead)
-def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(get_db)):
-    db_task = crud.update_task(db, task_id=task_id, task_update=task)
-    if db_task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return db_task
-
-# Delete a task
-@app.delete("/tasks/{task_id}", response_model=schemas.TaskRead)
-def delete_task(task_id: int, db: Session = Depends(get_db)):
-    db_task = crud.delete_task(db, task_id=task_id)
-    if  db_task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return db_task
-
-# Get all tasks
-@app.get("/tasks/", response_model=list[schemas.TaskRead])
-def  get_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    tasks = crud.get_tasks(db=db, skip=skip, limit=limit)
-    return tasks
-
-# Get a single task by ID
-@app.get("/tasks/{task_id}", response_model=schemas.TaskRead) 
-def get_task(task_id: int, db: Session = Depends(get_db)):
-    task = crud.get_task(db=db, task_id=task_id)
-    if task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return task
-    
+app.include_router(routes.router)
