20a1edeedabf175f45b72df5012c0d69b4e99405
idalz <idalez@outlook.com>
2025-04-18 23:17:38 +0300

    Removed front end files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import task, user, auth
 
 
 app = FastAPI()
+
+# Allow frontend to make requests
+origins = [
+    "http://localhost:3000",  # Your frontend URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(task.router)
 app.include_router(user.router)
