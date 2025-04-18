f3cb39a4c97be5585f80182d1e3b37bf27b8d9c7
idalz <idalez@outlook.com>
2025-04-18 21:23:06 +0300

    Add postgresql migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,20 +6,21 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.database import Base
+from app.models.task import Task
+from app.models.user import User
 
+import sys
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-# Get the DATABASE_URL from .env
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-config.set_section_option('alembic', 'sqlalchemy.url', DATABASE_URL)
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -30,7 +31,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = [Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
